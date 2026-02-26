f53e8a936ec66f8a0faa7630e90b5c2ecf57d5ba
karan singh <karanksrk2000@outlook.com>
2026-02-26 23:19:35 +0530

    added ollama

--- backend/chat_backend/settings.py
+++ backend/chat_backend/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'rest_framework',
     'corsheaders',
     'api',
@@ -125,7 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
 
 
 # LLM Configuration
